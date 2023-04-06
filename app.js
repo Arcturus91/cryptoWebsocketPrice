085b2e74f4a4a3f52a7c985644ff11ada653358b
Arcturus91 <arturo.barrantesv@pucp.pe>
2023-04-06 01:03:08 -0500

    Implement CORS policy for client in port 3000

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
+let numClients = 0;
 const Binance = require("node-binance-api");
 const binance = new Binance().options({
   APIKEY: process.env.APIKEY,
@@ -13,21 +21,41 @@ const binance = new Binance().options({
 });
 
 io.on("connection", (socket) => {
-  console.log("user connected");
+  /*TODO: tried to find a way to display all connected users but need more time:
+  const clientSocketMap = io.sockets.sockets;
+  const socketMapObject = Object.fromEntries(clientSocketMap);
+   console.log(Object.keys(socketMapObject)); 
+  console.log(socketMapObject[Object.keys(socketMapObject)[0]]); */
+
+  numClients++;
+  console.log("clients listening ", numClients);
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
+    numClients--;
   });
 
-  const data = binance.websockets.candlesticks(
+  const btcPrice = binance.websockets.candlesticks(
     ["BTCUSDT"],
     "1m",
     (candlesticks) => {
-      let { e: eventType, E: eventTime, s: symbol, k: ticks } = candlesticks;
-      let { c: close, i: interval } = ticks;
-      console.info(symbol + " " + interval + " candlestick update");
-      console.info("close: " + close);
-      io.emit("cryptoPrice", { symbol: symbol, price: close }); //it is key to have the emit event in the candlesticks arro function. This way we ensure the event goes out with the info.
+      let { s: symbol, k: ticks } = candlesticks;
+      let { c: close } = ticks;
+      let cryptoPrice = +close;
+      console.info(symbol + ": " + cryptoPrice);
+      io.emit("btcPrice", { symbol: symbol, price: cryptoPrice }); //it is key to have the emit event in the candlesticks arro function. This way we ensure the event goes out with the info.
+    }
+  );
+
+  const ethPrice = binance.websockets.candlesticks(
+    ["ETHUSDT"],
+    "1m",
+    (candlesticks) => {
+      let { s: symbol, k: ticks } = candlesticks;
+      let { c: close } = ticks;
+      let cryptoPrice = +close;
+      console.info(symbol + ": " + cryptoPrice);
+      io.emit("ethPrice", { symbol: symbol, price: cryptoPrice }); //it is key to have the emit event in the candlesticks arro function. This way we ensure the event goes out with the info.
     }
   );
 });
@@ -46,6 +74,43 @@ app.get("/spotPriceBTC", (req, res, next) => {
       return res.status(500).json({ errorMessage: error.message });
     });
 });
-server.listen(3000, () => {
-  console.log("server listening on port 3000");
+server.listen(8080, () => {
+  console.log("server listening on port 8080");
 });
+
+/* 
+router.get("/closeWebSocket", (req, res, next) => {
+  let log = false;
+  let endpoints = binance.websockets.subscriptions();
+  for (let endpoint in endpoints) {
+    if (log) console.log("Terminated ws endpoint: " + endpoint);
+    binance.websockets.terminate(endpoint);
+  }
+  res.status(200).json("web socket closed");
+}); */
+
+/* console.log(candlesticks);
+{
+  e: 'kline',
+  E: 1680754354677,
+  s: 'ETHUSDT',
+  k: {
+    t: 1680754320000,
+    T: 1680754379999,
+    s: 'ETHUSDT',
+    i: '1m',
+    f: 1123255101,
+    L: 1123255339,
+    o: '1897.21000000',
+    c: '1896.89000000',
+    h: '1897.22000000',
+    l: '1896.89000000',
+    v: '21.26500000',
+    n: 239,
+    x: false,
+    q: '40341.20849700',
+    V: '9.22500000',
+    Q: '17499.90564700',
+    B: '0'
+  }
+} */
